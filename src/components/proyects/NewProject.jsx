7f4125251493bb24353182c957cd604f5d29bd43
Sergio99778 <sergio4brandon@gmail.com>
2022-01-30 02:33:00 -0600

    Adding state to NewProject's form.

--- src/components/proyects/NewProject.jsx
+++ src/components/proyects/NewProject.jsx
@@ -1,18 +1,44 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const NewProject = () => {
+  const [project, setProject] = useState({
+    name: "",
+  });
+
+  const { name } = project;
+
+  const handleChange = (e) => {
+    setProject({
+      ...project,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //Validate project
+    //Add to state
+    //Restar form
+  };
+
   return (
     <Fragment>
       <button type="button" className="btn btn-block btn-primario">
         New project
       </button>
 
-      <form action="" className="formulario-nuevo-proyecto">
+      <form
+        action=""
+        className="formulario-nuevo-proyecto"
+        onSubmit={handleSubmit}
+      >
         <input
           type="text"
           className="input-text"
           placeholder="Project name"
           name="name"
+          value={name}
+          onChange={handleChange}
         />
         <button type="submit" className="btn btn-primario btn-block">
           Add project
